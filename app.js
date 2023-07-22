132799304283d77681d09536e380d8458e13649f
Kalkidan Getachew <110995815+Chaos-19@users.noreply.github.com>
2023-07-22 07:12:25 +0100

    Getting the quote from random quote api

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-/*function getRandomQuote() {
+var quote = "";
 
+
+function getRandomQuote() {
  fetch('https://api.quotable.io/random')
     .then((response) => {
       if (!response.ok) throw Error(response.statusText)
        return response.json()
     })
-    .then((quot) => console.log(quot))
+    .then((quot) => quot)
     .catch(error => console.log(error));
+}
 
-*/
 
 const bord = document.querySelector('.bord');
 let posi = 0;
 
 function writer() {
+if (quote) {
+  
+}
 
-  const word = 'There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.'
 
   if (word.length > posi) {
     bord.innerHTML = `${word.substring(0,posi)
