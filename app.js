f3a7f2ebc120e71f939ae71123dfb26960f344eb
kalgetachew375@gmail.com <kalgetachew375@gmail.com>
2023-07-23 15:19:03 +0300

    update to write the author name

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const bord = document.querySelector('.bord');
+const author = document.querySelector('.blockquote-footer')
 let index = 0;
-let responseTime = 0;
 let randomQuote = '';
+let quoteContent = '';
 
 async function getRandomQuote() {
   try {
     const response = await fetch('https://api.quotable.io/random');
     if (!response.ok) throw Error(response.statusText);
     const result = await response.json();
-    randomQuote = result.content;
+    randomQuote = result;
+    quoteContent = randomQuote.content;
   } catch (error) {
     console.log(error);
   }
@@ -16,17 +18,22 @@ async function getRandomQuote() {
 
 
 function writer() {
-  bord.innerHTML = `${randomQuote.substring(0,index)} <span>\u25AE</span>`
+  bord.innerHTML = `${quoteContent.substring(0,index)} <span>\u25AE</span>`
+
+  if (index < String(randomQuote.authorSlug).length){
+    author.innerHTML = `${String(randomQuote.authorSlug).substring(0,index)} <span>\u25AE</span>`;
+}
+
+
 
   if (randomQuote === '') {
     getRandomQuote();
-    console.log("true......");
   }
-  if (randomQuote.length > index) {
-    console.log("inner function ");
+  if (quoteContent.length > index) {
     setTimeout(writer, 100);
     index++;
-  } else if (randomQuote !== '' && randomQuote.length <= index) {
+  } else if (quoteContent !== '' && quoteContent.length <= index) {
+    quoteContent = '';
     randomQuote = '';
     index = 0;
     setTimeout(writer, 1500);
