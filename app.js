392542ad30fd2faeba79e0d66810aaac3c25ff79
Kalkidan Getachew <110995815+Chaos-19@users.noreply.github.com>
2023-07-23 12:08:16 +0100

    Update writer function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const bord = document.querySelector('.bord');
 let index = 0;
+let responseTime = 0;
 let randomQuote = '';
 
 async function getRandomQuote() {
@@ -14,25 +15,39 @@ async function getRandomQuote() {
 }
 
 
-
-
-
-
-
 function writer() {
-  
-  if (!randomQuote) {
-    getRandomQuote();
-  }
-  
   bord.innerHTML = `${randomQuote.substring(0,index)} <span>\u25AE</span>`
-  
-  if (randomQuote && randomQuote.length > index) {
-    setTimeout(write, 1000);
+
+  if (randomQuote === '') {
+    getRandomQuote();
+    console.log("true......");
+  }
+  if (randomQuote.length > index) {
+    console.log("inner function ");
+    setTimeout(writer, 100);
     index++;
-  }else{
-    writer();
+  } else if (randomQuote !== '' && randomQuote.length <= index) {
+    randomQuote = '';
+    index = 0;
+    setTimeout(writer, 1500);
+  } else {
+    setTimeout(writer, 2000);
   }
 }
-console.log(randomQuote);
-writer();
+
+window.addEventListener('load', writer);
+
+// find out the exact interval between each request  time 
+
+/*  
+
+async function checkResponseTime(testURL) {
+
+  let time1 = performance.now();
+  await fetch(testURL);
+  let time2 = performance.now();
+  return time2 - time1;
+}(async () => {
+  // for the purpose of this snippet example, some host with CORS response 
+responseTime = await checkResponseTime('https://api.quotable.io/random');
+})();
